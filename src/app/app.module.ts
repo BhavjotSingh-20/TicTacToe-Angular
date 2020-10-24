65336d56c3cd8899bc619527d6c827f6fa917462
Bhavjot Singh <bhavjot794@gmail.com>
2020-10-25 00:03:28 +0530

    Working

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+//Font
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+//Toastr
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
+ @NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
